18aad6ac12415ee1e54444412d6816f986b8cfc1
tsaketh <tsaketh1107@gmail.com>
2020-11-29 17:00:57 +0530

    Updates

---
+++ precache-manifest.d7a1cf2541b64580538ab8370eeee5f1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3c46875f644bc15f88cf2ab4681589",
+    "url": "/sentiment-analysis/index.html"
+  },
+  {
+    "revision": "c32aeafa80989b85c3da",
+    "url": "/sentiment-analysis/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "35afd8b326afb09c9def",
+    "url": "/sentiment-analysis/static/css/main.d52b0828.chunk.css"
+  },
+  {
+    "revision": "c32aeafa80989b85c3da",
+    "url": "/sentiment-analysis/static/js/2.e46b6eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sentiment-analysis/static/js/2.e46b6eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35afd8b326afb09c9def",
+    "url": "/sentiment-analysis/static/js/main.211cca67.chunk.js"
+  },
+  {
+    "revision": "f64c9a030e488cbb8bdf",
+    "url": "/sentiment-analysis/static/js/runtime-main.eaf035be.js"
+  },
+  {
+    "revision": "b68b1c651bab038eff6046413e7a9f1b",
+    "url": "/sentiment-analysis/static/media/Zilap Black Storm DEMO.b68b1c65.ttf"
+  },
+  {
+    "revision": "f890044b2c269424361453bf723b2d59",
+    "url": "/sentiment-analysis/static/media/emoji_0.f890044b.png"
+  },
+  {
+    "revision": "fad0d21863486e9fef44e02f642d2954",
+    "url": "/sentiment-analysis/static/media/emoji_1.fad0d218.png"
+  },
+  {
+    "revision": "5518e764c6bbce8ae4e1134e2c79a6c8",
+    "url": "/sentiment-analysis/static/media/emoji_2.5518e764.png"
+  },
+  {
+    "revision": "02543668b6881fcae92fd9cbc78f70de",
+    "url": "/sentiment-analysis/static/media/emoji_3.02543668.png"
+  },
+  {
+    "revision": "9f6dc92a4dd9c3517787deefff688e29",
+    "url": "/sentiment-analysis/static/media/emoji_4.9f6dc92a.png"
+  }
+]);
